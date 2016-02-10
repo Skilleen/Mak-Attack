3dec1d9784a31128234c1f206734ca1ff9b1a9b8
Raymond Chung <raymond_chung2@hotmail.com>
2016-02-10 18:48:35 -0500

    Added new things, please Scott overrride EVERYTHING in your unity

--- Mak Attack/Assets/Menu/Button.cs
+++ Mak Attack/Assets/Menu/Button.cs
@@ -4,25 +4,12 @@ using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
 public class Button : MonoBehaviour {
-	/*
-	// Use this for initialization
-	void Start () {
-		int count = 50000000;
-		while (count > 0) {
-			count--;
-		}
-		SceneManager.LoadScene("LevelOne");
-			
-	}
-	*/
-
 	// Update is called once per frame
 	void Update () {
 	}
 
 
 	public void onClick() {
-		Debug.Log ("got it to click");
 		SceneManager.LoadScene("LevelOne");
 	}
 }
