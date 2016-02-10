ad00532feddbca9cef9662367711ac466a619306
Raymond Chung <raymond_chung2@hotmail.com>
2016-02-09 21:00:42 -0500

    The menu is added but it does not work onClick

---
+++ Mak Attack/Assets/Menu/Button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int count = 50000000;
+		while (count > 0) {
+			count--;
+		}
+		SceneManager.LoadScene("LevelOne");
+			
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void onClick() {
+		SceneManager.LoadScene("LevelOne");
+	}
+}
---
+++ Mak Attack/Assets/start.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene("LevelOne");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
