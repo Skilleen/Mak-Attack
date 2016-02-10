8a00764c0379d9079968db62857526226073b52d
Raymond Chung <raymond_chung2@hotmail.com>
2016-02-10 18:12:10 -0500

    Fixed the button

--- Mak Attack/Assets/Menu/Button.cs
+++ Mak Attack/Assets/Menu/Button.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class Button : MonoBehaviour {
-
+	/*
 	// Use this for initialization
 	void Start () {
 		int count = 50000000;
@@ -13,12 +14,15 @@ public class Button : MonoBehaviour {
 		SceneManager.LoadScene("LevelOne");
 			
 	}
+	*/
 
 	// Update is called once per frame
 	void Update () {
 	}
 
+
 	public void onClick() {
+		Debug.Log ("got it to click");
 		SceneManager.LoadScene("LevelOne");
 	}
 }
