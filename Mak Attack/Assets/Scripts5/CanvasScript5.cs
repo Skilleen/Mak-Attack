f9d15097d1b5cbdb6218dba8e70f9df5466e0fa5
Scott Killeen <scott.killeen@hotmail.com>
2016-03-28 11:36:02 -0400

    Finished Level 5

---
+++ Mak Attack/Assets/Scripts5/CanvasScript5.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasScript5 : MonoBehaviour {
+
+    public Text instruction;
+    public int armourLevel;
+    private float timeLeft = 5.0f;
+    private bool bossPaid = false;
+    // Use this for initialization
+    void Start()
+    {
+        instruction = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject Rogue = GameObject.Find("Rogue");
+        RayoScriptLevel5 rayoScript = Rogue.GetComponent<RayoScriptLevel5>();
+        if (!rayoScript.dead)
+        {
+            instruction.text = "Health: " + rayoScript.rayoLife.ToString() + "/" + rayoScript.rayoMaxLife.ToString() + "         Score: " + rayoScript.rayoScore.ToString() + "           Potions: " + rayoScript.potionCount.ToString();
+        }
+        else
+        {
+            instruction.text = "You are dead! Your Final score is " + rayoScript.rayoScore.ToString("n2");
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("menu");
+            }
+        }
+    }
+}
---
+++ Mak Attack/Assets/Scripts5/RayoScriptLevel5.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RayoScriptLevel5 : MonoBehaviour {
+
+    Animator anim;
+    public float speed = 3.5f;
+    public bool facingRight = true;
+    public float rayoLife;
+    public int rayoScore;
+    public bool dead = false;
+    public int potionCount;
+    public float rayoDamage;
+    public int rayoMaxLife;
+    private bool bossCollide = false;
+    public bool rayoHealed = false;
+    private int count = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        PlayerPrefs.SetInt("gothotdog", 0);
+        rayoLife = PlayerPrefs.GetFloat("health");
+        rayoMaxLife = PlayerPrefs.GetInt("maxhealth");
+        rayoScore = PlayerPrefs.GetInt("score");
+        rayoDamage = PlayerPrefs.GetFloat("damage");
+        potionCount = PlayerPrefs.GetInt("potions");
+        PlayerPrefs.SetInt("level", 5);
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerPrefs.SetFloat("health", rayoLife);
+        PlayerPrefs.SetInt("maxhealth", rayoMaxLife);
+        PlayerPrefs.SetInt("score", rayoScore);
+        PlayerPrefs.SetFloat("damage", rayoDamage);
+        PlayerPrefs.SetInt("potions", potionCount);
+        GameObject boss = GameObject.Find("finalBoss");
+        finalBossScript bossScript = boss.GetComponent<finalBossScript>();
+        boss.GetComponent<SpriteRenderer>().color = Color.white;
+        anim.SetBool("attack", false);
+        anim.SetBool("walk", false); //reset animations on every update
+        if (!dead) //Make sure player is not dead
+        {
+
+            if (Input.GetKey(KeyCode.Q) && count <= 0)
+            {
+                if (potionCount > 0)
+                {
+                    potionCount--;
+                    rayoLife = rayoMaxLife;
+                    count = 100;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (facingRight)
+                {
+                    Vector3 theScale = transform.localScale;
+                    theScale.x *= -1;
+                    transform.localScale = theScale;
+                    facingRight = false;
+                }
+                transform.position += Vector3.left * speed * Time.deltaTime;
+                anim.SetBool("walk", true);
+                anim.SetBool("attack", false);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (!facingRight)
+                {
+                    Vector3 theScale = transform.localScale;
+                    theScale.x *= -1;
+                    transform.localScale = theScale;
+                    facingRight = true;
+                }
+                transform.position += Vector3.right * speed * Time.deltaTime;
+                anim.SetBool("walk", true);
+                anim.SetBool("attack", false);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += Vector3.up * speed * Time.deltaTime;
+                anim.SetBool("walk", true);
+                anim.SetBool("attack", false);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += Vector3.down * speed * Time.deltaTime;
+                anim.SetBool("walk", true);
+                anim.SetBool("attack", false);
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+
+                if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("shanking"))
+                {
+                    // Avoid any reload.
+                }
+                else {
+                    anim.SetBool("attack", true);
+                    if (bossCollide && !bossScript.knightDead && bossScript.doDamage)
+                    {
+                        boss.GetComponent<SpriteRenderer>().color = Color.red;
+                        bossScript.knightLife -= 4;
+                    }
+
+                }
+            }
+            count--;
+            if (bossScript.playerHit && bossScript.doDamage && !dead)
+            {
+                rayoLife = rayoLife - 0.5f;
+            }
+            if (rayoLife <= 0)
+            {
+                anim.SetBool("dead", true);
+                anim.SetBool("attack", false);
+                anim.SetBool("walk", false);
+                dead = true;
+                rayoLife = 0;
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+     
+        if (col.gameObject.name == "levelExit")
+        {
+            SceneManager.LoadScene("menu");
+        }
+        if (col.gameObject.name == "finalBoss")
+        {
+            bossCollide = true;
+            GameObject temp = GameObject.Find("enochonalex");
+            temp.AddComponent<BoxCollider2D>();
+        }
+    }
+}
